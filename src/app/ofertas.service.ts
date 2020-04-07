aac0d6b22c38e402047cc5e467d1d209dd0f7c1b
Bruno Pessoa <brunopessoa.melo@gmail.com>
2020-04-07 19:56:14 -0300

    criando um provider do tipo service para as ofertas sejam dinâmicas

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class OfertasService {
+public getOfertas(): Array<string>{
+let ofertas = ['Oferta1', 'Oferta2','Oferta3']
+return ofertas 
+}
+  constructor() { }
+
+}
+
